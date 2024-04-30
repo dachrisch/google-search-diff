a08b7a4b9749d91dbfbb1c192d7358ec6c2ba187
dachrisch <christian.daehn@gmail.com>
2024-04-30 14:17:27 +0200

    removed unused imports

--- lib/router.dart
+++ lib/router.dart
@@ -1,10 +1,7 @@
 import 'package:fimber/fimber.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:google_search_diff/_new/model/searchQuery.dart';
-import 'package:google_search_diff/_new/model/searchQueryStore.dart';
 import 'package:google_search_diff/_new/routes/routes.dart';
-import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,4 +12,3 @@ void main() async {
     DeviceOrientation.portraitDown
   ]).then((_) => runApp(const RouterApp()));
 }
-
