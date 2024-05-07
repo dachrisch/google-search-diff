9d68163da18e74db114f8d3106bc30ef40517768
dachrisch <christian.daehn@gmail.com>
2024-05-07 13:32:48 +0200

    swipe action to delete (and restore)

--- lib/_new/widget/query_card.dart
+++ lib/_new/widget/query_card.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:google_search_diff/_new/logger.dart';
 import 'package:google_search_diff/_new/model/queries_store.dart';
 import 'package:google_search_diff/_new/model/query_runs.dart';
 import 'package:google_search_diff/_new/routes/relative_route_extension.dart';
 import 'package:google_search_diff/_new/service/search_service.dart';
 import 'package:google_search_diff/_new/widget/animated_icon_button.dart';
 import 'package:google_search_diff/_new/widget/timer_mixin.dart';
+import 'package:logger/logger.dart';
 import 'package:provider/provider.dart';
 import 'package:relative_time/relative_time.dart';
 
@@ -19,7 +21,7 @@ class SingleQueryCard extends StatefulWidget {
 
 class _SingleQueryCard extends State<SingleQueryCard>
     with TimerMixin, TickerProviderStateMixin {
-  bool isSearching = false;
+  final Logger l = getLogger('QueryCard');
 
   @override
   Widget build(BuildContext context) {
@@ -27,43 +29,77 @@ class _SingleQueryCard extends State<SingleQueryCard>
     QueriesStoreModel searchQueriesStore = context.watch<QueriesStoreModel>();
     SearchService searchService = context.read<SearchService>();
 
-    return Card(
-      elevation: 4.0,
-      margin: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-      child: InkWell(
-          onTap: () => context.goRelativeWithId(queryRuns.query.queryId),
-          child: ListTile(
-              leading: Container(
-                decoration: const BoxDecoration(
-                    border: Border(
-                        right: BorderSide(width: 1.0, color: Colors.white))),
-                child: AnimatedRefreshIconButton(
-                    buttonKey: Key(
-                        'refresh-query-results-outside-button-${queryRuns.query.queryId.id}'),
-                    onPressed: () => searchService
-                        .doSearch(queryRuns.query)
-                        .then((run) => queryRuns.addRun(run))),
+    return Dismissible(
+        key: Key(queryRuns.hashCode.toString()),
+        direction: DismissDirection.endToStart,
+        background: Container(
+          margin: const EdgeInsets.all(10),
+          color: Colors.red,
+          child: const Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Icon(Icons.delete, color: Colors.white),
+              SizedBox(width: 8)
+            ],
+          ),
+        ),
+        onDismissed: (direction) {
+          // https://www.dhiwise.com/post/how-to-implement-flutter-swipe-action-cell-in-mobile-app
+
+          searchQueriesStore.remove(queryRuns);
+          // Show a snackbar with undo action
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('Query "${queryRuns.query.term}" removed'),
+              action: SnackBarAction(
+                label: 'Undo',
+                onPressed: () async {
+                  l.d('Restore $queryRuns');
+                  searchQueriesStore.add(queryRuns);
+                },
               ),
-              title: Text(queryRuns.query.term),
-              subtitle: Row(
-                children: [
-                  Text('Results: ${queryRuns.items}'),
-                  const SizedBox(
-                    width: 30,
+            ),
+          );
+        },
+        child: Card(
+          elevation: 4.0,
+          margin: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+          child: InkWell(
+              onTap: () => context.goRelativeWithId(queryRuns.query.queryId),
+              child: ListTile(
+                  leading: Container(
+                    decoration: const BoxDecoration(
+                        border: Border(
+                            right:
+                                BorderSide(width: 1.0, color: Colors.white))),
+                    child: AnimatedRefreshIconButton(
+                        buttonKey: Key(
+                            'refresh-query-results-outside-button-${queryRuns.query.queryId.id}'),
+                        onPressed: () => searchService
+                            .doSearch(queryRuns.query)
+                            .then((run) => queryRuns.addRun(run))),
                   ),
-                  Text(
-                      'Updated: ${queryRuns.items > 0 ? RelativeTime(context).format(queryRuns.latest.runDate) : 'N/A'}')
-                ],
-              ),
-              trailing: Container(
-                decoration: const BoxDecoration(
-                    border: Border(
-                        left: BorderSide(width: 1.0, color: Colors.white))),
-                child: IconButton(
-                    key: Key('delete-search-query-${queryRuns.query.queryId}'),
-                    icon: const Icon(Icons.delete),
-                    onPressed: () => searchQueriesStore.remove(queryRuns)),
-              ))),
-    );
+                  title: Text(queryRuns.query.term),
+                  subtitle: Row(
+                    children: [
+                      Text('Results: ${queryRuns.items}'),
+                      const SizedBox(
+                        width: 30,
+                      ),
+                      Text(
+                          'Updated: ${queryRuns.items > 0 ? RelativeTime(context).format(queryRuns.latest.runDate) : 'N/A'}')
+                    ],
+                  ),
+                  trailing: Container(
+                    decoration: const BoxDecoration(
+                        border: Border(
+                            left: BorderSide(width: 1.0, color: Colors.white))),
+                    child: IconButton(
+                        key: Key(
+                            'delete-search-query-${queryRuns.query.queryId}'),
+                        icon: const Icon(Icons.delete),
+                        onPressed: () => searchQueriesStore.remove(queryRuns)),
+                  ))),
+        ));
   }
 }
--- lib/_new/widget/run_card.dart
+++ lib/_new/widget/run_card.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:google_search_diff/_new/logger.dart';
 import 'package:google_search_diff/_new/model/comparison.dart';
 import 'package:google_search_diff/_new/model/query_runs.dart';
 import 'package:google_search_diff/_new/model/run.dart';
 import 'package:google_search_diff/_new/routes/relative_route_extension.dart';
 import 'package:google_search_diff/_new/widget/timer_mixin.dart';
+import 'package:logger/logger.dart';
 import 'package:provider/provider.dart';
 import 'package:relative_time/relative_time.dart';
 
@@ -15,75 +17,110 @@ class RunCard extends StatefulWidget {
 }
 
 class _RunCardState extends State<RunCard> with TimerMixin {
+  final Logger l = getLogger('RunCard');
+
   @override
   Widget build(BuildContext context) {
     RunModel run = context.read<RunModel>();
     QueryRunsModel queryRuns = context.watch<QueryRunsModel>();
     ResultComparison resultComparison =
         queryRuns.nextRecentTo(run).compareTo(run);
-    return Card(
-      elevation: 4.0,
-      margin: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-      child: InkWell(
-          onTap: () => context.goRelativeWithId(run.runId),
-          child: ListTile(
-            leading: const Icon(Icons.list_outlined),
-            title:
-                Text('Created: ${RelativeTime(context).format(run.runDate)}'),
-            subtitle: SizedBox(
-                width: 100,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Row(children: [
-                          const Icon(Icons.arrow_left_outlined,
-                              color: Colors.green),
-                          Text(resultComparison.added.length.toString())
-                        ])
-                      ],
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Row(
-                          children: [
-                            const Icon(Icons.arrow_left_outlined,
-                                color: Colors.grey),
-                            Text(resultComparison.existing.length.toString()),
-                            const Icon(Icons.arrow_right_outlined,color: Colors.grey),
+    return Dismissible(
+      key: Key(run.runId.id.toString()),
+      direction: DismissDirection.endToStart,
+      background: Container(
+        margin: const EdgeInsets.all(10),
+        color: Colors.red,
+        child: const Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Icon(Icons.delete, color: Colors.white),
+            SizedBox(width: 8)
+          ],
+        ),
+      ),
+      onDismissed: (direction) {
+        // https://www.dhiwise.com/post/how-to-implement-flutter-swipe-action-cell-in-mobile-app
 
-                          ],
-                        )
-                      ],
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Row(
-                          children: [
-                            Text(resultComparison.removed.length.toString()),
-                            const Icon(Icons.arrow_right_outlined,
-                                color: Colors.red),
-                          ],
-                        )
-                      ],
-                    ),
-                  ],
-                )),
-            trailing: Container(
-                decoration: const BoxDecoration(
-                    border: Border(
-                        left: BorderSide(width: 1.0, color: Colors.white))),
-                child: IconButton(
-                  key: Key('delete-query-results-${run.runId}'),
-                  icon: const Icon(Icons.delete),
-                  onPressed: () => queryRuns.removeRun(run),
-                )),
-          )),
+        queryRuns.removeRun(run);
+        // Show a snackbar with undo action
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: const Text('Run dismissed'),
+            action: SnackBarAction(
+              label: 'Undo',
+              onPressed: () async {
+                l.d('Restore $run');
+                queryRuns.addRun(run);
+              },
+            ),
+          ),
+        );
+      },
+      child: Card(
+        elevation: 4.0,
+        margin: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+        child: InkWell(
+            onTap: () => context.goRelativeWithId(run.runId),
+            child: ListTile(
+              leading: const Icon(Icons.list_outlined),
+              title:
+                  Text('Created: ${RelativeTime(context).format(run.runDate)}'),
+              subtitle: SizedBox(
+                  width: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Row(children: [
+                            const Icon(Icons.arrow_left_outlined,
+                                color: Colors.green),
+                            Text(resultComparison.added.length.toString())
+                          ])
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Row(
+                            children: [
+                              const Icon(Icons.arrow_left_outlined,
+                                  color: Colors.grey),
+                              Text(resultComparison.existing.length.toString()),
+                              const Icon(Icons.arrow_right_outlined,
+                                  color: Colors.grey),
+                            ],
+                          )
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Row(
+                            children: [
+                              Text(resultComparison.removed.length.toString()),
+                              const Icon(Icons.arrow_right_outlined,
+                                  color: Colors.red),
+                            ],
+                          )
+                        ],
+                      ),
+                    ],
+                  )),
+              trailing: Container(
+                  decoration: const BoxDecoration(
+                      border: Border(
+                          left: BorderSide(width: 1.0, color: Colors.white))),
+                  child: IconButton(
+                    key: Key('delete-query-results-${run.runId}'),
+                    icon: const Icon(Icons.delete),
+                    onPressed: () => queryRuns.removeRun(run),
+                  )),
+            )),
+      ),
     );
   }
 }
