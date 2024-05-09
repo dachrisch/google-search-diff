05c02f4fd0d587515f1b156e27f95781414716c7
dachrisch <christian.daehn@gmail.com>
2024-05-09 23:12:01 +0200

    drag and drop test

--- test/_new/widget/drag_runs_test.dart
+++ test/_new/widget/drag_runs_test.dart
@@ -16,12 +16,12 @@ import 'package:provider/provider.dart';
 import '../util/localstore_helper.dart';
 import '../util/service_mocks.dart';
 import '../util/test_provider.dart';
+import 'widget_tester_extension.dart';
 
 void main() {
   setUp(() => cleanupBefore(['.runs', '.queries']));
 
-  testWidgets(skip: true, 'Drags a run to the target',
-      (WidgetTester tester) async {
+  testWidgets('Drags a run to the target', (WidgetTester tester) async {
     Provider.debugCheckInvalidValueType = null;
     var queriesStore = QueriesStore(
         dbQueryService: MockDbQueriesService(),
@@ -34,44 +34,44 @@ void main() {
     await queriesStore.queryRuns[0]
         .addRun(Run(query, [Result(title: 'result 2')]));
 
-    await tester.pumpWidget(ScaffoldMultiProviderTestApp(
-      providers: [
-        Provider<QueryId>.value(value: queryId),
-        ChangeNotifierProvider<QueriesStore>.value(value: queriesStore),
-        Provider<SearchService>.value(value: LoremIpsumSearchService())
-      ],
-      scaffoldUnderTest: const QueryRunsPage(),
+    await tester.pumpWidget(MediaQuery(
+      data: const MediaQueryData(textScaler: TextScaler.linear(0.5)),
+      child: ScaffoldMultiProviderTestApp(
+        providers: [
+          Provider<QueryId>.value(value: queryId),
+          ChangeNotifierProvider<QueriesStore>.value(value: queriesStore),
+          Provider<SearchService>.value(value: LoremIpsumSearchService())
+        ],
+        scaffoldUnderTest: const QueryRunsPage(),
+      ),
     ));
 
     expect(find.byType(RunCard), findsNWidgets(2));
 
-    // grab the card
-    Offset firstResultOffset = tester.getCenter(find.byType(RunCard).first);
-    TestGesture baseGesture = await tester.startGesture(firstResultOffset);
-    await tester.pumpAndSettle();
-
+    TestGesture baseGesture =
+        await tester.grabCard<RunCard>((found) => found.first);
     // expect feedback
     expect(find.byType(RunFeedbackCard), findsOneWidget);
-
-    // check target
-    var baseDropTarget = find.byType(RunDragTarget).first;
-    Offset baseDropTargetOffset = tester.getCenter(baseDropTarget);
-
-    expect(
-        find.descendant(of: baseDropTarget, matching: find.byType(EmptyTarget)),
-        findsOneWidget);
-
-    // move to drop zone
-    await baseGesture.moveTo(baseDropTargetOffset,
-        timeStamp: Durations.extralong4);
-    await tester.pumpAndSettle();
-    await baseGesture.up();
-    await tester.pumpAndSettle();
-
+    var baseDropTarget =
+        await tester.moveCardToTarget<RunDragTarget, EmptyTarget>(
+            baseGesture, (found) => found.first);
     // expect dropped
     expect(
         find.descendant(
             of: baseDropTarget, matching: find.byType(TargetWithRun)),
+        findsOneWidget);
+
+    TestGesture currentGesture =
+        await tester.grabCard<RunCard>((found) => found.last);
+    // expect feedback
+    expect(find.byType(RunFeedbackCard), findsOneWidget);
+    var currentDropTarget =
+        await tester.moveCardToTarget<RunDragTarget, EmptyTarget>(
+            currentGesture, (found) => found.last);
+    // expect dropped
+    expect(
+        find.descendant(
+            of: currentDropTarget, matching: find.byType(TargetWithRun)),
         findsOneWidget);
   });
 }
--- test/_new/widget/widget_tester_extension.dart
+++ test/_new/widget/widget_tester_extension.dart
@@ -5,6 +5,8 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:google_search_diff/_new/logger.dart';
@@ -16,5 +18,27 @@ extension ButtonTap on WidgetTester {
     l.d('Tapping key $key');
     await tap(find.byKey(Key(key)));
     return pumpAndSettle(); // https://stackoverflow.com/questions/49542389/flutter-get-a-popupmenubuttons-popupmenuitem-text-in-unit-tests
+  }
+}
+
+extension Grab on WidgetTester {
+  FutureOr<TestGesture> grabCard<T extends Widget>(
+          Finder Function(Finder found) selector) =>
+      startGesture(getCenter(selector(find.byType(T)))).then((gesture) async {
+        await pumpAndSettle();
+        return gesture;
+      });
+
+  Future<Finder> moveCardToTarget<T, V>(
+      TestGesture gesture, Finder Function(Finder found) selector) async {
+    var dropTarget = selector(find.byType(T));
+    Offset dropTargetOffset = getCenter(dropTarget);
+
+    expect(find.descendant(of: dropTarget, matching: find.byType(V)),
+        findsOneWidget);
+    await gesture.moveTo(dropTargetOffset);
+    await gesture.up();
+    await pumpAndSettle();
+    return dropTarget;
   }
 }
