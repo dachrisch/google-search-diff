1a70f7a9981d3ae4aa1aae376f6b9c57e56d4756
dachrisch <christian.daehn@gmail.com>
2024-05-13 14:50:36 +0200

    make button smaller on scroll

--- lib/search/search_result_view.dart
+++ lib/search/search_result_view.dart
@@ -1,7 +1,9 @@
 import 'package:async_button_builder/async_button_builder.dart';
 import 'package:flutter/material.dart';
+import 'package:google_search_diff/logger.dart';
 import 'package:google_search_diff/model/run.dart';
 import 'package:google_search_diff/widget/result/result_card.dart';
+import 'package:logger/logger.dart';
 
 class SearchResultsView extends StatefulWidget {
   final Future<void> Function(Run results) onSave;
@@ -18,23 +20,54 @@ class SearchResultsView extends StatefulWidget {
 }
 
 class _SearchResultsViewState extends State<SearchResultsView> {
+  final ScrollController controller = ScrollController();
+  final Logger l = getLogger('result-view');
+  bool atTop = true;
+
+  @override
+  void initState() {
+    controller.addListener(() {
+      setState(() {
+        atTop = controller.offset == 0;
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: ListView.builder(
+            controller: controller,
             itemBuilder: (context, index) =>
                 ResultCard(result: widget.run.results[index]),
             itemCount: widget.run.items),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
         floatingActionButton: AsyncButtonBuilder(
             child: const Icon(Icons.add_outlined),
             onPressed: () async => await widget.onSave(widget.run),
             builder: (context, child, callback, buttonState) {
-              return FloatingActionButton.extended(
-                  key: const Key('add-search-query-button'),
-                  onPressed: callback,
-                  icon: child,
-                  label: const Text('Save Query'));
+              return AnimatedSwitcher(
+                switchInCurve: Curves.easeInOut,
+                switchOutCurve: Curves.fastOutSlowIn,
+                duration: Durations.extralong4,
+                transitionBuilder: (child, animation) => SlideTransition(
+                  position: Tween<Offset>(
+                          begin: const Offset(10, 0), end: const Offset(0, 0))
+                      .animate(animation),
+                  child: child,
+                ),
+                child: atTop
+                    ? FloatingActionButton.extended(
+                        key: const Key('add-search-query-button'),
+                        onPressed: callback,
+                        icon: child,
+                        label: const Text('Save Query'))
+                    : FloatingActionButton.small(
+                        key: const Key('add-search-query-button'),
+                        onPressed: callback,
+                        child: child),
+              );
             }));
   }
 }
